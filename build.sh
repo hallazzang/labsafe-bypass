1d1b547457d1fc3abd7d65752ec9ac0a20740b86
Hanjun Kim <hallazzang@gmail.com>
2018-06-13 01:11:51 +0900

    build: change gzip compression level to 9

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ echo windows/amd64
 GOOS=windows GOARCH=amd64 go build -o build/labsafe-windows.amd64.exe cmd/labsafe/*.go
 
 echo compressing
-tar -C build -cvzf build/labsafe-darwin.amd64.tar.gz labsafe-darwin.amd64
-tar -C build -cvzf build/labsafe-linux.amd64.tar.gz labsafe-linux.amd64
+GZIP=-9 tar -C build -cvzf build/labsafe-darwin.amd64.tar.gz labsafe-darwin.amd64
+GZIP=-9 tar -C build -cvzf build/labsafe-linux.amd64.tar.gz labsafe-linux.amd64
 cd build
 zip -9 -FS labsafe-windows.386.zip labsafe-windows.386.exe
 zip -9 -FS labsafe-windows.amd64.zip labsafe-windows.amd64.exe
